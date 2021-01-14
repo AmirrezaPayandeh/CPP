fed5259e2d1cc8e060d05fcff2df1bb8e9a83311
Amirreza <Blacklord.ap@gmail.com>
2021-01-14 15:02:00 +0330

    fixed some issues with ternary operators

--- SandBox/src/TernaryOperators/TernaryOperators.cpp
+++ SandBox/src/TernaryOperators/TernaryOperators.cpp
@@ -10,13 +10,9 @@ void TernaryOperatorsPrint()
 	if (Level > 5)
 		Speed = 10;
 	else
-		Speed = 50;
+		Speed = 5;
 
 	// you can write the exact same if statement as follow
 	Speed = Level > 5 ? 10 : 5;
-	std::cout << "Speed = " << Speed << std::endl;
-
-	// but remember to make it as simple as you can.
-	Speed = (Level > 10 && Level < 100) ? Level < 10 ?  : 10;
 	std::cout << "Speed = " << Speed << std::endl;
 }
